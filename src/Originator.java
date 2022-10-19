47c50fe43c0ddc98ce4eb6e1661e945ff6148f92
VARIANT 7 <Kislenko1950@gmail.com>
2022-10-19 17:28:47 +0300

    Initial commit

---
+++ src/Originator.java
@@ -0,0 +1,20 @@
+public class Originator {
+    private Stats state;
+
+    public Stats getState() {
+        return state;
+    }
+
+    public void setState(Stats state) {
+        this.state = state;
+    }
+
+    public Memento saveStateToMemento() {
+        return new Memento(state);
+    }
+
+    public void getStateFromMemento(Stats stats) {
+        this.state = stats;
+    }
+
+}
